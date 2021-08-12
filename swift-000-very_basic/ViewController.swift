84e7ca77d77b25682b2db38bab6d1ce746d2a46b
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-11 22:17:18 -0300

    Initial Commit

---
+++ swift-000-very_basic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-000-very_basic
+//
+//  Created by Luiz Carlos da Silva Araujo on 11/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
